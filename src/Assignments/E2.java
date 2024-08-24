e551f7055d46196ebdbc9b0ab6656e9a67240e2a
MADHAN <MADHAN@LAPTOP-AHBNHQLJ>
2024-08-24 11:22:15 +0530

    first commit

---
+++ src/Assignments/E2.java
@@ -0,0 +1,17 @@
+/*2. Write a program to print your name.*/
+
+package Assignments;
+
+public class E2 {
+	
+	public void display(String a)
+	{
+		System.out.print("name:"+a);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		E2 obj = new E2();
+		obj.display("Dyuthi");
+	}
+
+}
