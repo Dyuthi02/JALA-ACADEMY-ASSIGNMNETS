a439517a15fe05bbda0b9b020617e2e29f86410d
Dyuthi02 <dyuthisri02@gmail.com>
2024-08-26 12:58:40 +0530

    first commit

---
+++ PrintName.java
@@ -0,0 +1,15 @@
+//2. Write a program to print your name
+
+package JALA_ACADEMY_ASSIGNMNETS;
+
+public class PrintName {
+    public void print(String a)
+    {
+        System.out.print(a);
+    }
+    public static void main(String[]args)
+    {
+        PrintName obj=new PrintName();
+        obj.print("Dyuthi sri");
+    }
+}
