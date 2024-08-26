6aab09e747b30eda86e52e19348676cb3b968188
Dyuthi02 <dyuthisri02@gmail.com>
2024-08-26 15:34:56 +0530

    first commit

---
+++ Loops/BigIt.java
@@ -0,0 +1,13 @@
+//1. Write a program to print “Bright IT Career” ten times using for loop
+
+package JALA_ACADEMY_ASSIGNMNETS.Loops;
+
+public class BigIt
+{
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("Bright IT Career");
+        }
+    }
+}
