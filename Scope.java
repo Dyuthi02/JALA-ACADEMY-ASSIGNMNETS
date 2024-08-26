ced881f3b979c0f723df47d9da042edfe2bf9b61
Dyuthi02 <dyuthisri02@gmail.com>
2024-08-26 14:57:14 +0530

    first commit

--- Scope.java
+++ Scope.java
@@ -1,4 +1,5 @@
-//4. Define variables for different Data Types int, Boolean, char, float, double and print on the Console
+/*5. Define the local and Global variables with the same name and print both variables and
+understand the scope of the variables*/
 
 package JALA_ACADEMY_ASSIGNMNETS;
 
