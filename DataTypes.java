dfdb8723d96de8ea1a9357b9176ee785a61a29eb
Dyuthi02 <dyuthisri02@gmail.com>
2024-08-26 14:52:16 +0530

    first commit

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+//4. Define variables for different Data Types int, Boolean, char, float, double and print on the Console
+
+package JALA_ACADEMY_ASSIGNMNETS;
+
+public class DataTypes 
+{
+    public static void main(String[] args)
+     {
+        int i = 18;             // Default size is 4 byte
+        float f = 46.1f;            // Default size is 4 byte
+        double d = 189.46677;        // Default size is 8 byte
+        char c = 'D';             // Default size is 2 byte
+        boolean b = true;           // Default size is 1 byte
+
+        System.out.println("This is an Integer "+i);
+        System.out.println("This is a Float " + f);
+        System.out.println("This is a Double " + d);
+        System.out.println("This is a Char " + c);
+        System.out.println("This is a Boolean " + b);
+    }
+}
