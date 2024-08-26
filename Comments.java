0df3f1a21d57ddb5a7681f1592830ec2c1f76446
Dyuthi02 <dyuthisri02@gmail.com>
2024-08-26 14:47:03 +0530

    first commit

---
+++ Comments.java
@@ -0,0 +1,17 @@
+//3. Write a program for a Single line comment, multi-line and documentation comments
+
+package JALA_ACADEMY_ASSIGNMNETS;
+
+public class Comments
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("// This is a Single line comment");
+        
+
+        System.out.println("/* This is a Multi-line comment */");
+        
+
+        System.out.println("/** This is a Documentation comment */");
+    }
+}
